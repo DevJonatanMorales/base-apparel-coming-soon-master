04a546dfa0fb75d8d9804ae9425385b4b024954d
Jonatan Morales <moralesorellana170498@gmail.com>
2022-05-18 19:16:03 -0600

    validacion terminado :)

--- js/main.js
+++ js/main.js
@@ -7,18 +7,28 @@ const Expresiones = {
   correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9]+$/,
 };
 
+function classActive() {
+  icon.classList.add("icon__error-active");
+  lbAlert.classList.add("footer__alert-active");
+}
+
+function classDesactive() {
+  icon.classList.remove("icon__error-active");
+  lbAlert.classList.remove("footer__alert-active");
+}
+
 const validation = (value) => {
   if (Expresiones.correo.test(value)) {
-    icon.classList.remove("icon__error-active");
-    lbAlert.classList.remove("footer__alert-active");
+    classDesactive();
   } else {
-    icon.classList.add("icon__error-active");
-    lbAlert.classList.add("footer__alert-active");
+    classActive();
   }
 };
 
 btn.addEventListener("click", () => {
   if (txtEmail.value.length > 0) {
     validation(txtEmail.value);
+  } else {
+    classDesactive();
   }
 });
